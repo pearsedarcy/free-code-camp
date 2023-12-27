e83af1d4ac615cb40939ca12dfdafcc76c905c3b
pearsedarcy <pearse.darcy@outlook.com>
2023-12-27 17:32:02 +0100

    Add Step 34-36

--- javascript-essentials/project-1/script.js
+++ javascript-essentials/project-1/script.js
@@ -8,3 +8,18 @@ let inventory = ["stick", "dagger", "sword"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+// STEP 34 - Day 2
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+// STEP 35 - Day 2
+const monsterHealthText = document.querySelector("#monsterHealth");
+// STEP 36 - Day 2
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+// STEP 37 - Day 2
