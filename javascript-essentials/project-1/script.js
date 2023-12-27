ac6060a466ced51bf78d5621065c65f43162ad57
pearsedarcy <pearse.darcy@outlook.com>
2023-12-27 18:26:07 +0100

    Add location objects

--- javascript-essentials/project-1/script.js
+++ javascript-essentials/project-1/script.js
@@ -55,4 +55,21 @@ function buyWeapon() {}
 function buyHealth() {}
 // STEP 51 - Day 2
 function update(location) {}
-const locations = [{}];
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+];
